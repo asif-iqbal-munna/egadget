af29b66601738d084736335fa25f2d0ccf240dc7
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-10-16 14:13:25 +0600

    Main Header is completed

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  Container,
+  FormControl,
+  InputGroup,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
 
 const MainHeader = () => {
-  return <div></div>;
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container className="d-flex align-items-center">
+        <Navbar.Brand href="#home" className="text-white fs-3">
+          E-gadget
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <InputGroup className="w-50 mx-auto">
+            <FormControl
+              placeholder="Search Products"
+              aria-label="Search Products"
+              aria-describedby="basic-addon2"
+            />
+          </InputGroup>
+          <Nav className="ms-auto">
+            <i className="fas fa-shopping-cart fs-4 text-white me-5 cursor"></i>
+            <i className="fas fa-user fs-4 text-white cursor"></i>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default MainHeader;
--- src/components/Header/SubHeader.js
+++ src/components/Header/SubHeader.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const SubHeader = () => {
   return (
-    <div className="py-3 d-flex align-items-cente">
-      <p className="me-5 m-0">+88017077-42317</p>
-      <p className="m-0">contact.egadget@gmail.com</p>
+    <div className='d-flex align-items-center justify-content-between'>
+      <div className="py-3 d-flex align-items-center">
+        <p className="me-5 m-0">+88017077-42317</p>
+        <p className="m-0">contact.egadget@gmail.com</p>
+      </div>
+      <div>
+        <button className="btn btn-secondary">LogIn</button>
+      </div>
     </div>
   );
 };
